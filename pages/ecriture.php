255147661bfd81ad1fda1cb1a2cb27e2bd21f9cf
marechal230eme <marechal230eme@gmail.com>
2016-12-19 17:09:49 +0100

    changement pour le logo

--- pages/ecriture.php
+++ pages/ecriture.php
@@ -1,21 +1,17 @@
 <?php
-
 /*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
+  ode de la page crée le 19/12/2016 par pierre parrat
+  Nom de la page : accueil
+  But de la page : ecriture des articles
  */
-    session_start();
-    if ( (!isset($_SESSION['connecte'])) || (isset($_POST['deconnecte'])))
-    {
-        $_SESSION ['connecte'] = false; 
-        $_SESSION['pseudo'] = "";
-    }
-    if (!isset($_SESSION['pseudo']))
-    {
-        $_SESSION['pseudo'] = "";
-    }
-    
+session_start();
+if ((!isset($_SESSION['connecte'])) || (isset($_POST['deconnecte']))) {
+    $_SESSION ['connecte'] = false;
+    $_SESSION['pseudo'] = "";
+}
+if (!isset($_SESSION['pseudo'])) {
+    $_SESSION['pseudo'] = "";
+}
 ?>
 
 <html>
@@ -27,20 +23,24 @@
         <link href="../css/accueil.css" rel="stylesheet" type="text/css"/>
         <link href="../css/materialize.css" rel="stylesheet" type="text/css"/>
     </head>
-    
+
     <body>
-        <img class="logo" src="../images/Logo.png" alt="Logo du site"/>
-        
+        <header>
+            <img class="logoEcriture" src="../images/Logo.png" alt="Logo du site"/> 
+            
+        </header>
+
+
         <fieldset>
             <legend>Ecrire un article</legend>
-                <form method="post" action="verifArticle.php">
-                    <p>Titre : <input type="text" name="titre"></p>
-                    <p>Corps de l'article : <textarea name="corps">Votre article...</textarea></p>
-					<p>Tags :</p>
-					<p>CIR <input type="checkbox" name="tag" value="cir"></p>
-					<p>CSI <input type="checkbox" name="tag" value="csi"></p>
-                    <input type="submit" value="Valider">
-                </form>
+            <form method="post" action="verifArticle.php">
+                <p>Titre : <input type="text" name="titre"></p>
+                <p>Corps de l'article : <textarea name="corps">Votre article...</textarea></p>
+                <p>Tags :</p>
+                <p>CIR <input type="checkbox" name="tag" value="cir"></p>
+                <p>CSI <input type="checkbox" name="tag" value="csi"></p>
+                <input type="submit" value="Valider">
+            </form>
         </fieldset>
     </body>
     <script src="../js/materialize.js" type="text/javascript"></script>
