baf33a0bb242554caf6892e31cdc503156bb474d
marechal230eme <marechal230eme@gmail.com>
2016-12-19 16:24:07 +0100

    commencement du header

--- pages/ecriture.php
+++ pages/ecriture.php
@@ -1,8 +1,25 @@
 <?php
-
-/* 
+/*
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-echo"YOLO" ; 
+?>
+<html>
+    <head>
+        <title>Ynsay</title>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <meta name="description" content="Présentation de Ynsay,sformulaire de connexion/inscription" />
+        <link href="../css/accueil.css" rel="stylesheet" type="text/css"/>
+        <link href="../css/materialize.css" rel="stylesheet" type="text/css"/>
+    </head>
+    <header>
+      <img class="logo" src="../images/Logo.png" alt="Logo du site"/>
+    </header>
+    <body>
+        <h6>bonjour</h6>
+    </body>
+    <script src="../js/materialize.js" type="text/javascript"></script>
+</html>
+
