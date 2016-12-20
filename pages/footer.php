8bdacc5c3747c266afeeeb41d99d31e7fd134bc9
Antoine Berenguer <antoine.berenguer@isen.yncrea.fr>
2016-12-20 16:08:58 +0100

    modif de footer.php

--- pages/footer.php
+++ pages/footer.php
@@ -1,4 +1,9 @@
+﻿<footer>
 	<ul>
+		<li><a href="javascript:void(0);">Mention légales</a></li>
+		<li><a href="javascript:void(0);">A propos</a></li>
+		<li><a href="faq.php">FAQ</a></li>
+		<li><a href="javascript:void(0);">Contact email</a></li>
 	</ul>
 	<p>Copyright &copy; <?php echo date("Y"); ?> Ynsay</p>
 </footer>
