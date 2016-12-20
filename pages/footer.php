4d0d24323827638bc301e328c98077e2d626dcd4
Antoine Berenguer <antoine.berenguer@isen.yncrea.fr>
2016-12-20 16:08:11 +0100

    Modification diverses
    -Ajout de faq.php vide
    -Creation du footer (pas de CSS)
    -Rajout du footer dans ecriture.php
    -Déplacement de la balise header
    -rajout de commentaires dans la page verifArticle.php

---
+++ pages/footer.php
@@ -0,0 +1,4 @@
+	<ul>
+	</ul>
+	<p>Copyright &copy; <?php echo date("Y"); ?> Ynsay</p>
+</footer>
--- pages/verifArticle.php
+++ pages/verifArticle.php
@@ -1,7 +1,7 @@
-<!--
-Page cr�e le 19/12/2016 par Antoine Berenguer
+﻿<!--
+Page crée le 19/12/2016 par Antoine Berenguer
 Nom de la page : verifArticle
-But de la page : Verification de la validite d'un article
+But de la page : Verification de la validité d'un article
 -->
 
 <?php
@@ -9,28 +9,28 @@ But de la page : Verification de la validite d'un article
 	$formulaireValide = true;
 	if (( isset($_POST['titre']) AND empty($_POST['titre']) ) || ((strlen($_POST['titre'])) > 100)) {
 		$formulaireValide = false;
-		echo "Titre invalide! Vous devez renseigner un titre entre 1 et 100 caracteres.<br />";
+		echo "Titre invalide! Vous devez renseigner un titre entre 1 et 100 caractères.<br />";
 	}
 	
 	if (( isset($_POST['corps']) AND empty($_POST['corps']) ) || ((strlen($_POST['corps'])) > 10000)) {
 		$formulaireValide = false;
-		echo "Corps d'article invalide! Vous devez ecrire un article entre 1 et 10000 caracteres.<br />";
+		echo "Corps d'article invalide! Vous devez écrire un article entre 1 et 10000 caractères.<br />";
 	}
     
 	if ( $formulaireValide == true )
 	{
-		$titre = $_POST['titre'];
-		$corps = $_POST['corps'];
-		if(empty($tags))
+		$titre = $_POST['titre']; //On récupère le titre
+		$corps = $_POST['corps']; //On récupère le corps de l'article
+		if(empty($tags)) //Si aucun tag sélectionné
 		{
-			$listeTags = "general";
+			$listeTags = "general"; //On applique le tag général
 		}
-		else
+		else //Sinon
 		{
-			$tags = $_POST['tag'];
-			$listeTags = "";
-			$nbTags = count($tags);
-			for($i=0; $i < $nbTags; $i++)
+			$tags = $_POST['tag']; //Le tableau de tags est copié dans une variable
+			$listeTags = ""; //On initialise une liste de tags pour l'affichage
+			$nbTags = count($tags); //On récupère le nombre de tags choisis
+			for($i=0; $i < $nbTags; $i++) //On génère la liste de tags
 			{
 			  $listeTags = $listeTags.$tags[$i];
 			  if ($i != $nbTags - 1)
@@ -39,6 +39,7 @@ But de la page : Verification de la validite d'un article
 			  }
 			}
 		}
+		// Maintenant, on affiche l'article
 		echo "<h1>".$titre."</h1>";
 		echo "<p>".$corps."</p>";
 		echo "<p> tag(s) : ".$listeTags."</p>";
