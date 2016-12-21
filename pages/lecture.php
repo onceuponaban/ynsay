f67f5b984b5f8f0dc6f92a3dba88b4dd5804c8f5
marechal230eme <marechal230eme@gmail.com>
2016-12-21 15:10:42 +0100

    remise en forme de la page de lecture

--- pages/lecture.php
+++ pages/lecture.php
@@ -1,5 +1,6 @@
 <?php
-/* la page crée le 19/12/2016 par pierre parrat
+/*
+  la page crée le 19/12/2016 par pierre parrat
   Nom de la page : accueil
   But de la page : ecriture des articles
  */
@@ -19,8 +20,8 @@ if (!isset($_SESSION['pseudo'])) {
         <title>ecrire votre article </title>
         <meta charset="UTF-8">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <meta name="description" content="Page d'écriture des articles" />
-        <link href="../css/ecriture.css" rel="stylesheet" type="text/css"/>
+        <meta name="description" content="Page de lecture  des articles" />
+        <link href="../css/lecture.css" rel="stylesheet" type="text/css"/>
         <link href="../css/materialize.css" rel="stylesheet" type="text/css"/>
     </head>
 
@@ -30,41 +31,43 @@ if (!isset($_SESSION['pseudo'])) {
          <header>
         <!-- metre en place le logo a droite puis metre en place le nuage de tag et bouton envoyer dans le header  -->
         <img class="logo" src="../images/Logo.png" alt="Logo du site"/>
-        <fieldset>
-            <legend>liste des tags </legend>
-            <table>
+
+            <table class="tag">
+
                 <tr>
-                    <td>promo :</td>
-                    <td>cycle :</td>
-                    <td>cours :</td>
-                    <td>association :</td>
+                    <td><button onclick="">N1</button></td>
+                    <td><button onclick="">N2</button></td>
+                    <td><button onclick="">N3</button></td>
+                    <td><button onclick="">M1</button></td>
+                    <td><button onclick="">M2</button></td>
+                    <td><button onclick="">BDE</button></td>
                 </tr>
                 <tr>
-                    <td> <button onclick="">N1</button></td> 
-                    <td>  <button onclick="">CIR</button></td>
+                    <td><button onclick="">CIR</button></td>
+                    <td><button onclick="">CSI</button></td>
+                    <td><button onclick="">CNB</button></td>
+                    <td><button onclick="">IT2I</button></td>
+                    <td><button onclick="">BDS</button></td>
                 </tr>
                 <tr>
-                    <td> <button onclick="">N2</button></td> 
-                    <td>  <button onclick="">CSI</button></td>
+                    <td><button onclick="">General</button></td>
+                    <td><button onclick="">cours</button></td>
+                    <td><button onclick="">humour</button></td>
+                    <td><button onclick="">Pict'isen</button></td>
                 </tr>
                 <tr>
-                    <td> <button onclick="">N3</button></td> 
-                    <td>  <button onclick="">CNB</button></td>
+                    <td><button onclick="">BDS</button></td>
+                    <td><button onclick="">BC2I</button></td>
+                    <td><button onclick="">Partage</button></td>
                 </tr>
                 <tr>
-                    <td> <button onclick="">M1</button></td> 
-                    <td>  <button onclick="">IT2I</button></td>
+
+
                 </tr>
-                <tr>
-                    <td> <button onclick="">M2</button></td> 
-                    <td>  <button onclick=""></button></td>
-                </tr>
+
                 
+
             </table>
-
-                
-
-        </fieldset>
 
     </header>
 
