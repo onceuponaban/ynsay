d2eb09c087edad901b204c67893decb22aafb9ed
marechal230eme <marechal230eme@gmail.com>
2016-12-31 12:15:08 +0100

    changement dans ecriture

--- pages/ecriture.php
+++ pages/ecriture.php
@@ -26,12 +26,16 @@ include 'extention_de_code/verifSession.php' ?>
         <?php include 'profilUtilisateur.php';?>
         <fieldset>
             <legend>Ecrivez votre article</legend>
-            <form method="post" action="verifArticle.php">
+            <form method="post" action="lecture.php.php">
+                <?php include 'selectionTagEcriture.php'; ?>
                 <p>titre de votre article  <input type="text" name="titre"></p>
                 <p>Corps de votre article : <textarea name="corps">Votre article...</textarea></p>
                 <input class="btn waves-effect waves-light orange accent-4" type="submit" value="Valider">
             </form>
         </fieldset>
+
+        <?php include 'verifArticle.php' ; ?>
+
 		<?php include 'extention_de_code/footer.php';?>
     </body>
 </html>
